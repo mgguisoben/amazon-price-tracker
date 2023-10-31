6d8053a1526ab60b78e15621cf5d52df16bfd102
mgguisoben <mgguisoben@gmail.com>
2023-10-31 21:19:01 +0800

    Amazon price tracker done

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ TO_EMAIL = os.environ.get("TO_EMAIL")
 
 product_url = ("https://www.amazon.com/OSTRICH-ORIGINAL-Pillow-Airplanes-Accessories/dp/B00B4S6SLW/ref=sr_1_5?crid"
                "=ZT4TM3XHX6EI&keywords=ostrich+pillow&qid=1698756512&sprefix=ostrich+pill%2Caps%2C359&sr=8-5")
+# From http://myhttpheader.com/
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0",
     "Accept-Language": "en-US,en;q=0.5"
@@ -25,7 +26,8 @@ price_whole = soup.find(name="span", class_="a-price-whole").getText()
 price_fraction = soup.find(name="span", class_="a-price-fraction").getText()
 price = float(price_whole + price_fraction)
 
-if price > 70:
+# Check if price is below the price you want
+if price < 70:
     with smtplib.SMTP(host="smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
